c205b0cea37d8328bbbf35e8cfaa62fd1a520441
Course <course16062021@gmail.com>
2022-01-28 00:34:20 +0300

    auth fragment

--- app/src/main/java/com/example/vkapp/TokenData.kt
+++ app/src/main/java/com/example/vkapp/TokenData.kt
@@ -1,10 +1,34 @@
 package com.example.vkapp
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vkapp.const.VERSION
+import kotlinx.coroutines.launch
+import retrofit2.awaitResponse
 
-class TokenData: ViewModel() {
+class TokenData : ViewModel() {
+    var retrofitBuilder = RetrofitCreator().getUserAccessToken()
 
     val token = MutableLiveData<String>()
+
+    //Если вызывать из класса фрагмента, то выдает значение "k" как null
+    // см. комменты в AuthorizationFragment
+    fun retrofitResponse() {
+        viewModelScope.launch {
+
+            val response = retrofitBuilder.newsJSONResponse(
+                token.toString(),
+                VERSION
+            ).awaitResponse()
+
+            if (response.isSuccessful) {
+                val data = response.body()
+                val k = (data?.response?.items?.get(1)?.post_type).toString()
+                Log.d("CWW", k)
+            }
+        }
+    }
 
 }
